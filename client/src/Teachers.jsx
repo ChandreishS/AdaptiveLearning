ff7d293df984487b361c0ef7f311d51dae3f39bb
ChandreishS <chandreishs@gmail.com>
2025-09-08 22:38:49 +0530

    Add Teachers.jsx

---
+++ client/src/Teachers.jsx
@@ -0,0 +1,8 @@
+export default function Teachers() {
+  return (
+    <div className="container mt-5">
+      <h2>Teachers</h2>
+      <p>List of current teachers and their details will go here.</p>
+    </div>
+  );
+}
